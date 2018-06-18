bdaf27c43b51e59240a7eb6679fd4aec1b31b755
Nguyen Huu Trang <huutrangvn@gmail.com>
2018-06-18 17:58:19 +0700

    digital clock using timer

---
+++ Digital Clock dung Timer/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Digital_Clock_dung_Timer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(176, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digital Clock";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoEllipsis = true;
+            this.lblTime.AutoSize = true;
+            this.lblTime.BackColor = System.Drawing.Color.Black;
+            this.lblTime.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.ForeColor = System.Drawing.Color.Lime;
+            this.lblTime.Location = new System.Drawing.Point(184, 108);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(130, 33);
+            this.lblTime.TabIndex = 1;
+            this.lblTime.Text = "00:00:00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 306);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTime;
+    }
+}
+
---
+++ Digital Clock dung Timer/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Digital_Clock_dung_Timer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Timer t = new Timer();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            t.Interval = 1000;  // interval la 1s
+            t.Tick += new EventHandler(t_Tick);
+            t.Start();  // chay bo timer
+        }
+
+        private void t_Tick(object sender, EventArgs e)
+        {
+            int hh, mm, ss;
+            hh = DateTime.Now.Hour;
+            mm = DateTime.Now.Minute;
+            ss = DateTime.Now.Second;
+            string time = "";
+            if(hh<10)
+            {
+                time += "0" + hh.ToString();
+            }
+            else
+            {
+                time += hh.ToString();
+            }
+            time += ":";
+            if(mm<10)
+            {
+                time += "0" + mm.ToString();
+            }
+            else
+            {
+                time += mm.ToString();
+            }
+            time += ":";
+            if (ss<10)
+            {
+                time += "0" + ss.ToString();
+            }
+            else
+            {
+                time += ss.ToString();
+            }
+
+            lblTime.Text = time;
+        }
+    }
+}
